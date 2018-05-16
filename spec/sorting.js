c3d31646e415955e319ab4c5923bd50e345ecc5b
cristian <stafie.cristian@gmail.com>
2018-05-16 00:46:38 -0400

    cleanup

--- spec/sorting.js
+++ spec/sorting.js
@@ -1,33 +1,42 @@
-let sorting = require('./../src/sorting/merge-sort')
+let sorting = require("./../src/sorting/merge-sort");
 
-describe('SORT TESTING:', () => {
+describe("SORT TESTING:", () => {
+  let comp = (a, b) => a > b;
+  let sortAsc = (a, b) => a - b;
+  let sortDesc = (a, b) => b - a;
 
-	let comp = (a,b) => a > b;
+  let emptyArray = [];
+  let arrayOfLength1 = [0];
+  let evenLengthArray = [8, 1, 4, 3, 7, 5, 2, 6];
+  let oddLengthArray = [8, 1, 4, 3, 7, 5, 2, 6, 13, 10, 11, 9, 12];
 
-	describe('merge sort recursion', () => {
-	  it ('should sort the empty list', () => {
-	  	expect(sorting.mergeSortRecursion([])).toEqual([]);
-		});	
+  describe("merge sort recursion", () => {
+    it("should sort the empty list", () => {
+      expect(sorting.mergeSortRecursion(emptyArray)).toEqual(emptyArray);
+    });
 
-		it ('should sort a list of length 1', () => {
-	  	expect(sorting.mergeSortRecursion([0])).toEqual([0]);
-		});	
+    it("should sort a list of length 1", () => {
+      expect(sorting.mergeSortRecursion(arrayOfLength1)).toEqual(
+        arrayOfLength1
+      );
+    });
 
-		it ('should sort a list of numbers', () => {
-	  	expect(sorting.mergeSortRecursion([8,1,4,3,7,5,2,6]))
-	  		.toEqual([1,2,3,4,5,6,7,8]);
-		});	
+    it("should sort a list of numbers", () => {
+      expect(sorting.mergeSortRecursion(evenLengthArray)).toEqual(
+        evenLengthArray.sort(sortAsc)
+      );
+    });
 
-		it ('should sort a list of numbers of odd length', () => {
-	  	expect(sorting.mergeSortRecursion([8,1,4,3,7,5,2,6,13,10,11,9,12]))
-	  		.toEqual([1,2,3,4,5,6,7,8,9,10,11,12,13]);
-		});	
+    it("should sort a list of numbers of odd length", () => {
+      expect(sorting.mergeSortRecursion(oddLengthArray)).toEqual(
+        oddLengthArray.sort(sortAsc)
+      );
+    });
 
-		it ('should use a custom comparator', () => {
-	  	expect(sorting.mergeSortRecursion([2,3,4,1],comp))
-	  		.toEqual([4,3,2,1]);
-		});	
-	});
+    it("should use a custom comparator", () => {
+      expect(sorting.mergeSortRecursion(evenLengthArray, comp)).toEqual(
+        evenLengthArray.sort(sortDesc)
+      );
+    });
+  });
 });
-
-
